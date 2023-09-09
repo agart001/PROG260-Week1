67eb2bafa2d278710d434c94ac2e5192d1943623
agart001 <agart.art@gmail.com>
2023-09-08 21:52:41 -0500

    Add project files.

---
+++ PROG260-Week1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+using static PROG260_Week1.Utility;
+
+namespace PROG260_Week1
+{
+    internal class Program
+    {
+        /*
+         The number of games
+Which genre is the most frequent
+Which map names have the most number of characters excluding spaces, and which game they belong to
+A dictionary that uses the Id Property as a Key and Info object as a value, then display all the information using a loop
+The map names that have the letter z in them
+         */
+        public static GameInfo GI = new GameInfo();
+        static void Main(string[] args)
+        {
+            Spacer();
+            Print($"GameInfo Count: {GI.MetaData.Count + 1}", true);
+
+            List<string> genres = new List<string>();
+            GI.MetaData.ForEach(game => genres.Add(game.Genre));
+            
+            Spacer();
+            Print($"Most Frequent Genre: {ListMostFrequent<string>(genres)}", true);
+
+            List<string> maps = new List<string>();
+            GI.MetaData.ForEach(game => maps.AddRange(game.MapNames.ToList()));
+            string mostcharsmap = ListMostChars<string>(maps);
+
+            Spacer();
+            Print($"Most Char Map/Game: {mostcharsmap} / {GI.MetaData.Find(game => game.MapNames.Contains(mostcharsmap)).Name}", true);
+
+
+            Dictionary<int, Info> infodict = new Dictionary<int, Info>();
+            GI.MetaData.ForEach(game => infodict.Add(game.Id, game));
+
+            Spacer();
+            Print("Info Dict Display", true);
+            Spacer();
+
+            foreach(Info value in infodict.Values)
+            {
+                DisplayInfo(value);
+            }
+
+            List<string> zmaps = maps.FindAll(map => map.Contains('z'));
+
+            Print($"Maps that contain 'Z': {ListToString<string>(zmaps)} ", true);
+
+            Spacer();
+
+            //GI.MetaData.ForEach(game )
+
+            //Print($"Longest Map Name and Game of Origin: ")
+        }
+    }
+
+}
---
+++ PROG260-Week1/Utility.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG260_Week1
+{
+    internal static class Utility
+    {
+        public static void Print(string input, bool newline)
+        {
+            Console.Write(input);
+
+            if (newline) Console.WriteLine();
+        }
+
+        public static void Spacer() => Print("----------------------------------", true);
+
+        public static string ListToString<S>(List<S> list)
+        {
+            string liststring = "";
+            foreach (S s in list)
+            {
+                liststring += $"{s} ";
+            }
+
+            return liststring;
+        }
+
+        public static int GetLetterCount(dynamic input)
+        {
+            char[] chars = input.ToCharArray();
+            return chars.Where(c => c != ' ').Count();
+        }
+
+
+        public static S ListMostFrequent<S>(List<S> list)
+        {
+            List<S> unique = list.Distinct().ToList();
+
+            int[] counts = new int[unique.Count];
+            int index = 0;
+
+            foreach (S el in unique)
+            {
+                counts[index] = list.FindAll(g => g.Equals(el)).Count;
+                index++;
+            }
+
+            return unique[Array.IndexOf(counts, counts.Max())];;
+        }
+
+        public static S ListMostChars<S>(List<S> list)
+        {
+            int[] charcounts = new int[list.Count];
+
+            int index = 0;
+            foreach (S el in list)
+            {
+                charcounts[index] = GetLetterCount(el);
+                index++;
+            }
+            return list[Array.IndexOf(charcounts, charcounts.Max())]; ;
+        }
+
+        public static void DisplayInfo(Info info)
+        {
+            Print($"Name: {info.Name}", true);
+            Print($"Genre: {info.Genre}", true);
+
+            Print("Maps:", false);
+            int index = 0;
+            foreach(string map in info.MapNames)
+            {
+                Print($" {map}", false);
+                if(index == info.MapNames.Length-1) Print(Environment.NewLine, false);
+                index++;
+            }
+
+            Spacer();
+        }
+
+
+    }
+}
